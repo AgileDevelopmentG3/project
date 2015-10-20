352edbb2a637c4289822d3eae1e8137fda71f05d
JessLovesCSS <jessbrisson@gmail.com>
2015-10-20 15:24:56 -0500

    alkj

--- createprofile.php
+++ createprofile.php
@@ -56,23 +56,8 @@
             </div>
         </div>
         <div class="overlay"></div>
-        <div class="container">
-<<<<<<< HEAD
-            <form class="form-signin" action="newAccount.php" method="post">
-                <h2 class="form-signin-heading">Please sign in</h2>
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" name="txtEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" id="inputPassword" name="txtUserPassword" class="form-control" placeholder="Password" required>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                    </label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-            </form>
-=======
-            	<div class="panel-body">
+            <div class="container">
+            	   <div class="panel-body">
 			    		<form role="form">
 			    			<div class="row">
 			    				<div class="col-xs-6 col-sm-6 col-md-6">
@@ -108,8 +93,7 @@
 			    		
 			    		</form>
 			    	</div>
->>>>>>> eaf2859d6114f19cea27e2af82d11354e9543e1d
-        </div>
+            </div>
         <footer>
             <div class="container">
                 <div class="row">
