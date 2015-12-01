8c32674df49bf0750d7815cb472d18c441f0de8b
shileski <shileski@gmail.com>
2015-12-01 06:08:04 -0600

    Facebook embed re-added

--- index.php
+++ index.php
@@ -34,6 +34,16 @@
         </script>
     </head>
     <body>
+    <!--FACEBOOK EMBED JAVASCRIPT BEGIN-->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!--FACEBOOK EMBED JAVASCRIPT END-->
         <div class="aboveslider--red">
             <div class="container">
                 <div class="row aboveslider__text bottom-spacer">
@@ -265,9 +275,21 @@
 						?>
                        </ul>
                     </div>
+                    
                 </div>
             </div>
         </div> <!-- END FLUID CONTAINER -->
+        <div class="background-image">
+            <div class="container">
+                <div class="row container-spacer home__titles">
+        <div class="col-md-6">
+                    <!--FACEBOOK EMBED BEGIN-->
+                    <div class="fb-page" data-href="https://www.facebook.com/powerhouse.comics/?fref=ts" data-width="500" data-height="350" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/powerhouse.comics/?fref=ts"><a href="https://www.facebook.com/powerhouse.comics/?fref=ts">Power House Comics</a></blockquote></div></div>
+                    <!--FACEBOOK EMBED END-->
+                    </div>
+                    </div>
+                    </div>
+                    </div>
         <div class="map-container">
             <div class="map-overlay">
                 <div>
